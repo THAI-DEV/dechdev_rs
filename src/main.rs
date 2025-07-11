5f8790a779216dd67044afe64026e2d6d11e35e3
Ruangdech  Kurunat <ruangdech@gmail.com>
2025-07-12 01:10:56 +0700

    Refactor encryption functions: simplify variable handling and improve error handling in app_crypto_ase_gcm and app_crypto_aes

--- src/main.rs
+++ src/main.rs
@@ -114,18 +114,19 @@ fn app_crypto_ase_gcm() {
     let plain_text = "This is a secret message";
     let secret_key = "password_123";
 
-    let mut encrypted_text = "".to_string();
+    // let mut encrypted_text = "".to_string();
 
     println!("--- AES Encryption/Decryption ---");
-    match crypto_ase_gcm::encrypt_string(plain_text, secret_key) {
+    let encrypted_text = match crypto_ase_gcm::encrypt_string(plain_text, secret_key) {
         Ok(encrypted) => {
             println!("Encrypted: {encrypted}");
             println!("Encrypted Text: Success");
-            encrypted_text = encrypted;
+            encrypted
         }
         Err(e) => {
             println!("Encryption failed: {e}");
             println!("Encrypted Text: Failed");
+            return;
         }
     };
 
@@ -147,17 +148,16 @@ fn app_crypto_aes() {
     let plain_text = "This is test program";
     let secret_key = "fds13rpowhls59wnpahmtps112456789"; // 32 characters for AES-256
 
-    let mut encrypted_text = "".to_string();
-
-    match crypto_aes::encrypt_string(plain_text, secret_key) {
+    let encrypted_text = match crypto_aes::encrypt_string(plain_text, secret_key) {
         Ok(str1) => {
             println!("{str1}"); // Should output: YHPtkP6cNt5bZoi6vzxnWYn9Pq2CfjMi+siz/N6uYQQ=
-            encrypted_text = str1;
+            str1
         }
         Err(e) => {
             println!("Encryption failed: {e}");
+            return;
         }
-    }
+    };
 
     match crypto_aes::decrypt_string(&encrypted_text, secret_key) {
         Ok(str2) => {
