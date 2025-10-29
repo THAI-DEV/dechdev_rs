4a59f3b7e62a18b7121edd59d25d0917409203d5
Ruangdech Kurunat <ruangdech@gmail.com>
2025-10-30 01:03:46 +0700

    Bump version to 0.1.19; update executable path function and refactor file locating logic

--- src/example/ex_path_file.rs
+++ src/example/ex_path_file.rs
@@ -1,7 +1,9 @@
+use crate::utils::path_file;
+
 pub fn example_path_file() {
-    let current_path = crate::utils::path_file::get_current_path();
-    let execute_path = crate::utils::path_file::get_execute_path();
+    let current_path = path_file::get_current_path();
+    let executable_path = path_file::get_executable_path();
 
     println!("Current Path: {}", current_path);
-    println!("Execute Path: {}", execute_path);
+    println!("Execute Path: {}", executable_path);
 }
--- src/utils/path_file.rs
+++ src/utils/path_file.rs
@@ -5,7 +5,7 @@ pub fn get_current_path() -> String {
     current_dir.display().to_string().replace("\\", "/")
 }
 
-pub fn get_execute_path() -> String {
+pub fn get_executable_path() -> String {
     let exe_path = std::env::current_exe()
         .unwrap()
         .parent()
@@ -14,34 +14,35 @@ pub fn get_execute_path() -> String {
     exe_path.display().to_string().replace("\\", "/")
 }
 
-pub fn locate_file_from_path(
-    file_name: &str,
-    primary_path: &str,
-    secondary_path: &str,
-    display_info: bool,
-) -> String {
-    let file_path = {
-        let cwd_candidate = path::Path::new(&primary_path).join(file_name);
-        if cwd_candidate.exists() {
-            cwd_candidate.to_string_lossy().to_string()
-        } else {
-            let exe_candidate = path::Path::new(&secondary_path).join(file_name);
-            if exe_candidate.exists() {
-                exe_candidate.to_string_lossy().to_string()
-            } else {
-                String::new() // Return an empty string or handle the fallback case
-            }
+/// Searches for a file in a list of directories and returns its full path and parent directory.
+///
+/// # Arguments
+///
+/// * `file_name` - The name of the file to locate.
+/// * `path_list` - A vector of directory paths to search.
+///
+/// # Returns
+///
+/// A tuple containing:
+/// - The full path with file name to the located file as a `String`. If the file is not found, this will be an empty string.
+/// - The full path without file name to the located file as a `String`. If the file is not found, this will be an empty string.
+///
+/// ```
+pub fn locate_file_in_path(file_name: &str, path_list: Vec<&str>) -> (String, String) {
+    let mut file_path = String::new();
+    for path in path_list {
+        let candidate = path::Path::new(path).join(file_name);
+        if candidate.exists() {
+            file_path = candidate.to_string_lossy().to_string();
+            break;
         }
-    };
-
-    if display_info {
-        let file_path_without_name = path::Path::new(&file_path)
-            .parent()
-            .unwrap_or_else(|| path::Path::new(""))
-            .to_string_lossy()
-            .to_string();
-        println!("Use {} from {}", file_name, file_path_without_name);
     }
 
-    file_path
+    let file_path_without_name = path::Path::new(&file_path)
+        .parent()
+        .unwrap_or_else(|| path::Path::new(""))
+        .to_string_lossy()
+        .to_string();
+
+    (file_path, file_path_without_name)
 }
