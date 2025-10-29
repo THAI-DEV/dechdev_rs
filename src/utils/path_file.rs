7d6d26cfa29a2b2b4690e144700afe9527ed4e50
Ruangdech Kurunat <ruangdech@gmail.com>
2025-10-29 21:35:23 +0700

    Bump version to 0.1.17; refactor path_file utility functions and add locate_file_in_directories

--- src/utils/path_file.rs
+++ src/utils/path_file.rs
@@ -1,5 +1,7 @@
+use std::{env, path};
+
 pub fn get_current_path() -> String {
-    let current_dir = std::env::current_dir().unwrap();
+    let current_dir = env::current_dir().unwrap();
     current_dir.display().to_string().replace("\\", "/")
 }
 
@@ -10,4 +12,36 @@ pub fn get_execute_path() -> String {
         .unwrap()
         .to_path_buf();
     exe_path.display().to_string().replace("\\", "/")
+}
+
+pub fn locate_file_in_directories(
+    file_name: &str,
+    primary_path: &str,
+    secondary_path: &str,
+    display_info: bool,
+) -> String {
+    let file_path = {
+        let cwd_candidate = path::Path::new(&primary_path).join(file_name);
+        if cwd_candidate.exists() {
+            cwd_candidate.to_string_lossy().to_string()
+        } else {
+            let exe_candidate = path::Path::new(&secondary_path).join(file_name);
+            if exe_candidate.exists() {
+                exe_candidate.to_string_lossy().to_string()
+            } else {
+                String::new() // Return an empty string or handle the fallback case
+            }
+        }
+    };
+
+    if display_info {
+        let file_path_without_name = path::Path::new(&file_path)
+            .parent()
+            .unwrap_or_else(|| path::Path::new(""))
+            .to_string_lossy()
+            .to_string();
+        println!("Use {} from {}", file_name, file_path_without_name);
+    }
+
+    file_path
 }
