43731d58690858a2d07363d8b2a24c263c27eaf6
Ruangdech Kurunat <ruangdech@gmail.com>
2025-10-30 08:56:06 +0700

    Bump version to 0.1.20; refactor path_file utility functions and update example usage

--- src/example/ex_path_file.rs
+++ src/example/ex_path_file.rs
@@ -1,8 +1,8 @@
 use crate::utils::path_file;
 
 pub fn example_path_file() {
-    let current_path = path_file::get_current_path();
-    let executable_path = path_file::get_executable_path();
+    let current_path = path_file::current_path();
+    let executable_path = path_file::executable_path();
 
     println!("Current Path: {}", current_path);
     println!("Execute Path: {}", executable_path);
--- src/utils/path_file.rs
+++ src/utils/path_file.rs
@@ -1,11 +1,11 @@
 use std::{env, path};
 
-pub fn get_current_path() -> String {
+pub fn current_path() -> String {
     let current_dir = env::current_dir().unwrap();
     current_dir.display().to_string().replace("\\", "/")
 }
 
-pub fn get_executable_path() -> String {
+pub fn executable_path() -> String {
     let exe_path = std::env::current_exe()
         .unwrap()
         .parent()
@@ -14,21 +14,7 @@ pub fn get_executable_path() -> String {
     exe_path.display().to_string().replace("\\", "/")
 }
 
-/// Searches for a file in a list of directories and returns its full path and parent directory.
-///
-/// # Arguments
-///
-/// * `file_name` - The name of the file to locate.
-/// * `path_list` - A vector of directory paths to search.
-///
-/// # Returns
-///
-/// A tuple containing:
-/// - The full path with file name to the located file as a `String`. If the file is not found, this will be an empty string.
-/// - The full path without file name to the located file as a `String`. If the file is not found, this will be an empty string.
-///
-/// ```
-pub fn locate_file_in_path(file_name: &str, path_list: Vec<&str>) -> (String, String) {
+pub fn locate_file_path(file_name: &str, path_list: Vec<&str>) -> String {
     let mut file_path = String::new();
     for path in path_list {
         let candidate = path::Path::new(path).join(file_name);
@@ -38,11 +24,9 @@ pub fn locate_file_in_path(file_name: &str, path_list: Vec<&str>) -> (String, St
         }
     }
 
-    let file_path_without_name = path::Path::new(&file_path)
+    path::Path::new(&file_path)
         .parent()
         .unwrap_or_else(|| path::Path::new(""))
         .to_string_lossy()
-        .to_string();
-
-    (file_path, file_path_without_name)
+        .to_string()
 }
