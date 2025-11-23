350e5061cff9689b9635047261e3085f5980c4bb
Ruangdech Kurunat <ruangdech@gmail.com>
2025-11-23 21:31:03 +0700

    Add slice utility functions and update example usage in ex_slice

--- src/example/ex_slice.rs
+++ src/example/ex_slice.rs
@@ -1,5 +1,6 @@
 use crate::utils::slice::{
-    combine_slice, difference_slice, find_index, intersect_slice, union_slice,
+    combine_slice, difference_slice, find_index, intersect_slice, mutate_remove_slice,
+    mutate_reverse_slice, mutate_sort_slice, remove_slice, reverse_slice, sort_slice, union_slice,
 };
 
 pub fn example_slice() {
@@ -23,4 +24,26 @@ pub fn example_slice() {
 
     let indices = find_index(&vec3, &target);
     println!("find_index of '{}' in {:?} : {:?}", target, vec3, indices);
+
+    let r = reverse_slice(&vec1);
+    println!("reverse : {:?}", r);
+
+    let vec4 = ["f", "b", "c", "d", "a"];
+    let r = sort_slice(&vec4, false);
+    println!("sort_slice : {:?}", r);
+
+    let vec5 = ["f", "b", "c", "d", "a"];
+    let r = remove_slice(&vec5, &[1, 3]);
+    println!("remove_slice : {:?}", r);
+
+    let mut vex6 = ["f", "b", "c", "d", "a"];
+    mutate_reverse_slice(&mut vex6);
+    println!("mutate_reverse_slice : {:?}", vex6);
+
+    mutate_sort_slice(&mut vex6, true);
+    println!("mutate_sort_slice : {:?}", vex6);
+
+    let mut v4 = vec!["f", "b", "c", "d", "a"];
+    mutate_remove_slice(&mut v4, &[3, 1, 0]);
+    println!("mutate_remove_slice : {:?}", v4);
 }
--- src/utils/slice.rs
+++ src/utils/slice.rs
@@ -1,3 +1,5 @@
+use std::fmt::Debug;
+
 pub fn union_slice<T: PartialEq + Clone>(slice1: &[T], slice2: &[T]) -> Vec<T> {
     let mut result = Vec::new();
 
@@ -48,4 +50,69 @@ pub fn find_index<T: PartialEq>(v: &[T], target: &T) -> Vec<usize> {
         .enumerate()
         .filter_map(|(index, item)| if *item == *target { Some(index) } else { None })
         .collect()
+}
+
+pub fn reverse_slice<T: Copy>(slice: &[T]) -> Vec<T> {
+    let len = slice.len();
+    let mut result = Vec::with_capacity(len);
+    for i in (0..len).rev() {
+        result.push(slice[i]);
+    }
+
+    result
+}
+
+pub fn sort_slice<T: Copy + Debug>(slice: &[T], is_sorted_ascending: bool) -> Vec<T> {
+    let mut result = slice.to_vec();
+    if is_sorted_ascending {
+        result.sort_by(|a, b| format!("{:?}", a).cmp(&format!("{:?}", b)));
+    } else {
+        result.sort_by(|a, b| format!("{:?}", b).cmp(&format!("{:?}", a)));
+    }
+    result
+}
+
+pub fn remove_slice<T: Copy>(slice: &[T], index_list: &[usize]) -> Vec<T> {
+    //remove duplicate value in index_list
+    let mut unique_indices: Vec<usize> = index_list.to_vec();
+    unique_indices.sort_unstable();
+    unique_indices.dedup();
+
+    let mut result = Vec::new();
+    for (i, item) in slice.iter().enumerate() {
+        if !unique_indices.contains(&i) {
+            result.push(*item);
+        }
+    }
+    result
+}
+
+pub fn mutate_sort_slice<T: Copy + Debug>(slice: &mut [T], is_sorted_ascending: bool) {
+    if is_sorted_ascending {
+        slice.sort_by(|a, b| format!("{:?}", a).cmp(&format!("{:?}", b)));
+    } else {
+        slice.sort_by(|a, b| format!("{:?}", b).cmp(&format!("{:?}", a)));
+    }
+}
+
+pub fn mutate_reverse_slice<T: Copy>(slice: &mut [T]) {
+    let len = slice.len();
+    for i in 0..len / 2 {
+        slice.swap(i, len - 1 - i);
+    }
+}
+
+pub fn mutate_remove_slice<T: Copy>(slice: &mut Vec<T>, index_list: &[usize]) {
+    //remove duplicate value in index_list
+    let mut unique_indices: Vec<usize> = index_list.to_vec();
+    unique_indices.sort_unstable();
+    unique_indices.dedup();
+
+    let mut indices: Vec<usize> = unique_indices;
+    indices.sort_unstable_by(|a, b| b.cmp(a)); // sort indices in descending order
+    for &index in indices.iter() {
+        if index < slice.len() {
+            slice.remove(index);
+        }
+    }
 }
